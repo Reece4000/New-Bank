59d095713ed18751fb9c84c6c01fd1890367d262
lij3438 <lij3438@gmail.com>
2022-07-23 15:42:11 +0100

    add 3 new tests for testing user login security.

--- NewBank/src/test/java/newbank/server/NewBankTest.java
+++ NewBank/src/test/java/newbank/server/NewBankTest.java
@@ -1,15 +1,38 @@
 package newbank.server;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 class NewBankTest {
 
+    @DisplayName("User can view their accounts")
     @Test
     void testShowAccount() {
         NewBank testBank =  new NewBank(DefaultCustomers.create());
         String testRequest = testBank.processRequest(new CustomerID("Bhagy"), "SHOWMYACCOUNTS");
         Assertions.assertEquals(testRequest, "Main: 1000.0  ");
+    }
+    @DisplayName("User can login successfully")
+    @Test
+    void testUserLoginSuccess() {
+        NewBank testBank = new NewBank(DefaultCustomers.create());
+        CustomerID testCustomer = testBank.checkLogInDetails("Bhagy", "bhag");
+        Assertions.assertEquals(testCustomer.getKey(),"Bhagy");
+    }
+    @DisplayName("User cannot login with unknown username")
+    @Test
+    void testUserLoginWithUnknownUsername() {
+        NewBank testBank = new NewBank(DefaultCustomers.create());
+        CustomerID testCustomer = testBank.checkLogInDetails("Chagy", "bhag");
+        Assertions.assertEquals(testCustomer.getKey(),"Invalid username");
+    }
+    @DisplayName("User cannot login with unknown password")
+    @Test
+    void testUserLoginWithUnknownPassword() {
+        NewBank testBank = new NewBank(DefaultCustomers.create());
+        CustomerID testCustomer = testBank.checkLogInDetails("Bhagy", "bag");
+        Assertions.assertEquals(testCustomer.getKey(),"Invalid password");
     }
 }
